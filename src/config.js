64bd8ee68a59e91c46bbfad1fabce52dafd07f80
MIMIEYES <pierreluohao@gmail.com>
2021-12-16 11:43:31 +0800

    test settings

--- src/config.js
+++ src/config.js
@@ -12,10 +12,10 @@ export const RUN_PATTERN = false;
 export let API_URL = localStorage.hasOwnProperty("urls") ? JSON.parse(localStorage.getItem("urls")).urls : 'http://beta.public1.nuls.io/';
 setInterval(() => {
   API_URL = localStorage.hasOwnProperty("urls") ? JSON.parse(localStorage.getItem("urls")).urls : 'http://beta.public1.nuls.io/';
-}, 500);
+}, 600);
 //正式、测试网络的api
-export let LOCALHOST_API_URL = "http://"+window.location.host+"/offlineSmartContract";
-//export let LOCALHOST_API_URL = "http://127.0.0.1:8081/offlineSmartContract";
+// export let LOCALHOST_API_URL = "http://"+window.location.host+"/offlineSmartContract";
+export let LOCALHOST_API_URL = "http://192.168.1.39:18088/offlineSmartContract";
 export let PARAMETER = {jsonrpc: '2.0', method: '', params: [], id: 5898};
 
 
